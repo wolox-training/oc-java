24a8a2c779ab5223df57735cc4b13305a0d06fdc
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2020-06-10 17:26:44 -0300

    modified user model

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -2,6 +2,8 @@ package wolox.training.models;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
@@ -19,11 +21,16 @@ public class User {
 
     @Column(nullable = false)
     private LocalDate birthday;
-
-    private List<Book> books;
+    // PREGUNTAR: Cómo es el uso del one to one en este caso que solo estaria en el User ya que Books no tiene users, para combinar la tabla Books en esta.
+    private List<Book> books = new ArrayList<>();
 
     public User(long id) {
         this.id = id;
+    }
+
+    // PREGUNTAR: Debería books llevar un setter también ? "ya que luego será cargado automáticamente cuando se levante de la base de datos"
+    public List<Book> getBooks() {
+        return Collections.unmodifiableList(books);
     }
 
     public long getId() {
