55860809216f727f640b484e10b8477e7ba59f2c
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2020-07-20 20:25:02 -0300

    fix repository

--- src/main/java/wolox/training/repositories/UsersRepository.java
+++ src/main/java/wolox/training/repositories/UsersRepository.java
@@ -4,13 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import wolox.training.models.Users;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByUsername(String username);
-
-    List<Users> findFirstByUsername(String username);
 
 }
