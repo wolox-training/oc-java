f3733f17df274bd46a4bc30f43790b278cf99cc5
oriana.colmenares <oriana.colmenares@wolox.com.ar>
2020-07-29 14:20:06 -0300

    add bookDTO base

---
+++ src/main/java/wolox/training/dtos/BookDTO.java
@@ -0,0 +1,75 @@
+package wolox.training.dtos;
+
+import java.awt.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class BookDTO {
+    private String isbn;
+    private String title;
+    private String subtitle;
+    private String publisher;
+    private LocalDate publishDate;
+    private int numberOfPages;
+    private ArrayList<List> authors;
+
+
+    public BookDTO() {
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(LocalDate publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public ArrayList<List> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(ArrayList<List> authors) {
+        this.authors = authors;
+    }
+}
--- src/main/java/wolox/training/services/OpenLibraryService.java
+++ src/main/java/wolox/training/services/OpenLibraryService.java
@@ -1,5 +1,25 @@
 package wolox.training.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OpenLibraryService {
+   /* @GET
+    private Response findCustomer(@PathParam("bookIsbn") String bookIsbn) {
+        Book book = BookRepository.findBookByIsbn(bookIsbn); //Entity
+
+        //Create dto
+        BookDTO dto = new BookDTO();
+        dto.setIsbn(book.getIsbn());
+        dto.setTitle(book.getTitle());
+        dto.setSubtitle(book.getSubtitle());
+        dto.setPublisher(book.getPublisher());
+        dto.setNumberOfPages(book.getNumberOfPages());
+
+        //Return DTO
+        return Response.ok(dto, MediaType.APPLICATION_JSON).build();
+    }
+
+    */
 
 }
